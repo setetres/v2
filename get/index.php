3de04b2fe42a9e61291715e71456370396aec162
unknown <guilherme.pangnotta@JCDWEB01.JChebly.local>
2013-11-21 11:39:55 -0200

    Add project

---
+++ get/index.php
@@ -0,0 +1,58 @@
+<?php
+    error_reporting(E_ALL ^ E_NOTICE);
+    ini_set('display_errors', FALSE);
+?>
+<!doctype html>
+<html lang="en" itemscope itemtype="http://schema.org">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Sete Três - http://setetres.st"/>
+        <meta name="keywords" content=""/>
+
+        <title>Please upgrade to a modern browser</title>
+        <meta name="description" content="Your browser is no longer supported. Please upgrade to a modern browser."/>
+        <link rel="canonical" href=""/>
+
+        <meta itemprop="name" content="Please upgrade to a modern browser"/>
+        <meta itemprop="description" content="Your browser is no longer supported. Please upgrade to a modern browser."/>
+        <meta itemprop="image" content=""/>
+        <meta itemprop="url" content=""/>
+
+        <link rel="icon" href="favicon.ico"/>
+        <link rel="stylesheet" href="../css/normalize.css">
+        <link rel="stylesheet" href="../css/main.css"/>
+        <link rel="stylesheet" href="../css/main-responsive.css"/>
+
+        <script src="js/modernizr.js"></script>
+    </head>
+    <body>
+        <div id="wr">
+            <header>
+                <div class="center">
+                    <h1 class="logo">Please upgrade to a modern browser</h1>
+                    <nav role="navigation">
+                        <ul>
+                            <li title="Chrome"><a href="http://google.com/chrome" rel="external">Chrome</a></li>
+                            <li title="Internet Explorer"><a href="http://internetexplorer9.com" rel="external">Internet Explorer</a></li>
+                            <li title="Firefox"><a href="http://mozilla.com/firefox" rel="external">Firefox</a></li>
+                            <li title="Safari"><a href="http://apple.com/safari" rel="external">Safari</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <div id="main" role="main" class="wr">
+                <div class="center">
+                </div>
+            </div>
+            <footer>
+                <div class="center">
+                </div>
+            </footer>
+        </div>
+        <script src="js/jquery.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/functions.js"></script>
+        <script src="js/grid.js"></script>
+<? include("../inc/analytics.php");?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,100 @@
+<?php
+    error_reporting(E_ALL ^ E_NOTICE);
+    ini_set('display_errors', FALSE);
+
+    $uas = explode(';',$_SERVER['HTTP_USER_AGENT']);
+    $enable = 9;
+
+    foreach($uas as $ua) {
+        $ievs = strstr($ua, 'MSIE') ? floatval(substr($ua, 5,strlen($ua))) : NULL;
+    }
+
+    if(!is_null($ievs) && $ievs < $enable) {
+        header("Location: /get");
+    }
+?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" itemscope itemtype="http://schema.org"><!--<![endif]-->
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Sete Três - http://setetres.st"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width">
+
+        <title></title>
+        <meta name="description" content=""/>
+        <link rel="canonical" href=""/>
+
+        <meta property="og:type" content=""/>
+        <meta property="og:title" content=""/>
+        <meta property="og:description" content=""/>
+        <meta property="og:image" content=""/>
+        <meta property="og:url" content=""/>
+
+        <meta itemprop="name" content=""/>
+        <meta itemprop="description" content=""/>
+        <meta itemprop="image" content=""/>
+        <meta itemprop="url" content=""/>
+
+        <link rel="icon" href="favicon.ico"/>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css"/>
+        <link rel="stylesheet" href="css/main-responsive.css"/>
+
+        <script src="js/modernizr.js"></script>
+    </head>
+    <body>
+        <div id="fb-root"></div>
+        <div id="wr" class="wr">
+            <header>
+                <div class="center">
+                    <h1 class="logo"><a href="/">Title</a></h1>
+                    <nav role="navigation">
+                        <ul>
+                            <li><a href="#">Item #1</a></li>
+                            <li><a href="#">Item #2</a></li>
+                            <li><a href="#">Item #3</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <div id="main" role="main" class="main">
+                <div class="center">
+                    <div class="media-crop">
+                        <div class="crop">
+                            <img src="http://placekitten.com/200/300">
+                        </div>
+                    </div>
+                    <div class="media-crop custom">
+                        <div class="crop">
+                            <img src="http://placekitten.com/200/300">
+                        </div>
+                    </div>
+                    <div class="media-crop square">
+                        <div class="crop">
+                            <img src="http://placekitten.com/200/300">
+                        </div>
+                    </div>
+                    <div class="media-crop circle">
+                        <div class="crop">
+                            <img src="http://placekitten.com/200/300">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer>
+                <div class="center">
+                    <p>Copyright</p>
+                </div>
+            </footer>
+        </div>
+        <script src="js/jquery.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/functions.js"></script>
+        <script src="js/grid.js"></script>
+<? include("inc/analytics.php");?>
+    </body>
+</html>
