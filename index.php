ac1b51106b4442e3b9f41e499ee54b6b13c2166a
Guilherme <setetres@gmail.com>
2013-11-30 20:17:59 -0200

    Remove /inc and change error path

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php
+<?
     error_reporting(E_ALL ^ E_NOTICE);
-    ini_set('display_errors', FALSE);
+    ini_set('display_errors', TRUE);
 ?>
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
@@ -87,6 +87,18 @@
         <script src="js/plugins.js"></script>
         <script src="js/functions.js"></script>
         <script src="js/grid.js"></script>
-<? include("inc/analytics.php");?>
+        <script>
+
+            // analytics
+
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'XX-XXXXXXXX-X', 'domain.com');
+            ga('send', 'pageview');
+
+        </script>
     </body>
 </html>
